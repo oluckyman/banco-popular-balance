aa33d454938ff0eb11eace0d290f94e25c8f6876
ilya <ilya@methodexists.com>
2017-10-22 17:48:21 +0200

    Use Casper to login and fetch balance

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-var page = require('webpage').create();
+var url = 'https://www4.bancopopular.es/eai_logon/GbpInternetLogonEAI/gbplogon';
+var loggedInUrl = 'https://www4.bancopopular.es/eai_desktop/GbpInternetDesktop/EstablishSession#/CUE9001C_PosicionIntegral'
+var formSelector = '#identifica';
+var amountSelector = '#CUE9001C_PosicionIntegral1_sdoCta_0_amount';
 
-page.onConsoleMessage = function(msg) {
-  console.log('Page title is ' + msg);
-};
-
-page.open('https://www4.bancopopular.es/eai_logon/GbpInternetLogonEAI/gbplogon', function(status) {
-  console.log("Status: " + status);
-  if(status === "success") {
-    onSuccess();
-  }
-  phantom.exit();
+var casper = require('casper').create({
+  pageSettings: {
+    loadImages: false,
+    loadPlugins: false,
+  },
 });
 
+var username = casper.cli.get('username');
+var password = casper.cli.get('password');
 
-function onSuccess () {
-  var title = page.evaluate(function() {
-    var username = document.getElementById('username');
-    var pass = document.getElementById('userpass');
-    console.log(username, pass)
-  });
+if (!username || !password) {
+  console.error('Username and password are required');
+  casper.exit();
 }
+
+casper.start(url, function () {
+  this.waitForSelector(formSelector);
+});
+
+casper.then(function () {
+  this.fill(formSelector, {
+    username: username,
+    userpass: password,
+  }, true);
+  this.clickLabel('Entrar', 'button');
+});
+
+casper.waitForUrl(loggedInUrl, function () {
+  this.waitForSelector(amountSelector);
+  this.echo(this.fetchText(amountSelector));
+}, function () { this.capture('timeout.png'); }, 20000);
+
+casper.run();
