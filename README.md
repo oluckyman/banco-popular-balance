4e1f473e9f71ed2a70b96ebd8efca585622f1d60
ilya <ilya@methodexists.com>
2017-11-17 11:06:27 +0100

    Show show and save fetch timestamp

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+Banco Popular balance fetcher
+=============================
+Logs in into Banco Popular account and fetches current balance.
+Shows the result in the console and puts it into a file.
+
 Run
 ===
 ```
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var moment = require('moment');
 var url = 'https://www4.bancopopular.es/eai_logon/GbpInternetLogonEAI/gbplogon';
 var loggedInUrl = 'https://www4.bancopopular.es/eai_desktop/GbpInternetDesktop/EstablishSession#/CUE9001C_PosicionIntegral'
 var formSelector = '#identifica';
@@ -34,9 +35,9 @@ casper.then(function () {
 casper.waitForUrl(loggedInUrl, function () {
   this.waitForSelector(amountSelector);
   var amount = this.fetchText(amountSelector);
-  // this.echo(this.fetchText(amountSelector));
-  fs.write('amount.txt', amount, 'w');
-  console.log("Amount was saved: ", amount);
+  var message = '€' + amount + ' @ ' + moment().format("ddd Do h:mm A");
+  fs.write('amount.txt', message, 'w');
+  console.log("Saved:\n", message);
 }, function () { this.capture('timeout.png'); }, 20000);
 
 casper.run();
